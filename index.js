24aa3291b67967d2b4c2433e5e4dc31ba80e9cc7
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-09 13:56:14 -0300

    método para deletar

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ mongoose.connect(process.env.MONGO_URL);
 const Cat = mongoose.model('Cat', { name: String });
 
 const gato = await Cat.findById('6a28415ea020ed99b88392ff');
-gato.name = 'Garfield';
-await gato.save();
+await gato.deleteOne();
